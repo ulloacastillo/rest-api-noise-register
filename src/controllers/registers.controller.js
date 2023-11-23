60cb31f55ed675c03eee7e9805f30a573c098bac
Jorge Ulloa Castillo <ulloacastillo@MacBook-Air-de-Jorge.local>
2023-11-22 23:36:29 -0300

    chile date format

--- src/controllers/registers.controller.js
+++ src/controllers/registers.controller.js
@@ -2,9 +2,36 @@ import { pool } from "../db.js";
 
 export const getRegisters = async (req, res) => {
   try {
-    console.log("Hola");
-    const [rows] = await pool.query("select * from register");
-    res.send({ rows });
+    const [rows] = await pool.query(
+      "select * from register order by dateregister desc;"
+    );
+    // Fecha en formato GMT
+
+    // Ajustar la hora a la zona horaria de Chile (GMT -3)
+    let opciones = {
+      timeZone: "America/Santiago",
+      hour12: false,
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+
+    // Crear un objeto formateador de fecha y hora
+    let formateador = new Intl.DateTimeFormat("es-CL", opciones);
+
+    let data = rows.map((req) => {
+      return {
+        ...req,
+        dateregister: formateador.format(
+          new Date(req.dateregister.setHours(req.dateregister.getHours() - 3))
+        ),
+      };
+    });
+
+    res.send({ data });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: "Algo va mal" });
